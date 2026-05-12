482ee3d475ee68d2e82f0b66d5e4abe34abc5ef4
Mayanks2101 <mjsatapara@gmail.com>
2026-05-13 02:21:07 +0530

    ci: add packages in requirements-ci for Jenkins test execution

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
+import sys
+from unittest.mock import MagicMock
+
+sys.modules['sentence_transformers'] = MagicMock()
+sys.modules['groq'] = MagicMock()
+
 from fastapi.testclient import TestClient
 from src.api.app import app
 
